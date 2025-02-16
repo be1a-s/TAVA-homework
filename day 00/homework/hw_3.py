0f05ffbe762ae88c89b5d6206b822b210de399b8
be1a-s <beqaabuladze2009@gmail.com>
2025-02-16 14:37:02 +0400

    second commit

---
+++ day 00/homework/hw_3.py
@@ -0,0 +1,28 @@
+#ვქმნი რამოდენიმე ტიპის ცვლადს 
+name = ("beqa")
+
+surname = ("abuladze")
+
+coordinate = ("xulo aghmashenebelis qucha nomeri 7")
+
+age = (15)
+
+weight = (55)
+
+height = (1.61)
+
+hobby = ("fexburti")
+
+# ვპრინტავ გრძელი წინადადებას სამი ტიპის ცვლადით ჩემს შესახებ
+
+print( "me var " + surname + " " + name + " chemi sacxovrebeli adgilia " + coordinate + " var " + str(age) + " wlis " + str(weight) + " kilo " " chemi simaghlea " + str(height) + " chemi hobia " + hobby    )
+
+
+
+
+
+
+
+
+
+
